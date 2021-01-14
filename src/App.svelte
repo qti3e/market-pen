5a3596b2a0b1dc168fccc90181d800055bb08c6c
Parsa Ghadimi <qti3eqti3e@gmail.com>
2021-01-14 19:57:49 +0330

    Setup svelte and tailwindcss

---
+++ src/App.svelte
@@ -0,0 +1,20 @@
+<style global>
+  /* purgecss start ignore */
+  @tailwind base;
+  @tailwind components;
+  /* purgecss end ignore */
+
+  @tailwind utilities;
+</style>
+
+<script lang="ts">
+  export let name: string;
+</script>
+
+<main>
+  <h1>Hello {name}!</h1>
+  <p class="p-2">
+    Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte
+    apps.
+  </p>
+</main>
