6a7d9dd2118fdf7644ba372dfe1d7746f6a21fdb
Parsa Ghadimi <qti3eqti3e@gmail.com>
2021-01-17 10:04:32 +0330

    Add more logical methods and indicators

--- core/api/operation.ts
+++ core/api/operation.ts
@@ -1,3 +1,6 @@
+import * as indicators from '../indicators';
+import { assert } from '../util';
+
 export type Numeric = number | Operation;
 
 export abstract class Operation {
@@ -95,6 +98,13 @@ export abstract class Operation {
    */
   trunc(): Operation {
     return new MathOperation([this], Math.trunc);
+  }
+
+  /**
+   * Return `Floor(x / other) * other`.
+   */
+  step(other: Numeric): Operation {
+    return new MathOperation([this, other], MathOperation.step);
   }
 
   /**
@@ -239,8 +249,7 @@ export abstract class Operation {
 
   /**
    * Returns the square root of the sum of squares of its this number plus the ones provided in arguments.
-   *     If no arguments are passed, the result is +0.
-   *     If there is only one argument, the result is the absolute value.
+   *     If no arguments are passed, the result is the absolute value of current number.
    *     If any argument is +Infinity or -Infinity, the result is +Infinity.
    *     If any argument is NaN, the result is NaN.
    *     If all arguments are either +0 or −0, the result is +0.
@@ -292,6 +301,15 @@ export abstract class Operation {
   }
 
   /**
+   * `defaultValue.switchCase(condition1, value1, condition2, value2, ...)`.
+   * @param values Pairs of `condition` and `result`.
+   */
+  switchCase(...values: Numeric[]): Operation {
+    assert(values.length % 2 === 0 && values.length > 0, 'Wrong number of arguments.');
+    return new MathOperation([this, ...values], MathOperation.switchCase);
+  }
+
+  /**
    * Returns `1` if the current number is less than `other`, otherwise returns `0`.
    */
   lt(other: Numeric): Operation {
@@ -327,24 +345,61 @@ export abstract class Operation {
   }
 
   /**
-   * Returns `1` if the current number is positive to `other`, otherwise returns `0`.
+   * Returns `1` if the current number is positive (x > 0), otherwise returns `0`.
    */
   isPos(other: Numeric): Operation {
     return new MathOperation([this, 0], MathOperation.gt);
   }
 
   /**
-   * Returns `1` if the current number is positive to `other`, otherwise returns `0`.
+   * Returns `1` if the current number is negative (x < 0), otherwise returns `0`.
    */
   isNeg(other: Numeric): Operation {
     return new MathOperation([this, 0], MathOperation.lt);
   }
 
   /**
-   * Returns `1` if the current number is positive to `other`, otherwise returns `0`.
+   * Returns `1` if the current number is equal to `0`, otherwise returns `0`.
    */
   isZero(other: Numeric): Operation {
     return new MathOperation([this, 0], MathOperation.eq);
+  }
+
+  /**
+   * Negate the number as a boolean, if the number is truthy the value `0` is returned
+   * otherwise `1` is returned.
+   */
+  not(): Operation {
+    return new MathOperation([this], MathOperation.not);
+  }
+
+  /**
+   * Perform a logical `and` operation, the result is `1` or `0`.
+   * @param other Other numbers.
+   */
+  and(...other: Numeric[]): Operation {
+    return new MathOperation([this, ...other], MathOperation.and);
+  }
+
+  /**
+   * Perform a logical `or` operation, the result is `1` or `0`.
+   * @param other Other numbers.
+   */
+  or(...other: Numeric[]): Operation {
+    return new MathOperation([this, ...other], MathOperation.or);
+  }
+
+  /**
+   * Access point for technical analysis indicators.
+   */
+  get ta(): NumericIndicators {
+    return new NumericIndicators(this);
+  }
+}
+
+export class Candle extends Operation {
+  get ta(): CandleStickIndicators {
+    return new CandleStickIndicators(this);
   }
 }
 
@@ -386,6 +441,10 @@ export class MathOperation extends Operation {
     return result;
   }
 
+  static step(x: number, y: number) {
+    return Math.trunc(x / y) * y;
+  }
+
   static neg(n: number) {
     return -n;
   }
@@ -410,6 +469,22 @@ export class MathOperation extends Operation {
     return x === y ? 1 : 0;
   }
 
+  static and(...numbers: number[]) {
+    let result = true;
+    for (let i = 0, n = numbers.length; result && i < n; ++i) result = !!numbers[i];
+    return result ? 1 : 0;
+  }
+
+  static or(...numbers: number[]) {
+    let result = false;
+    for (let i = 0, n = numbers.length; !result && i < n; ++i) result = !!numbers[i];
+    return result ? 1 : 0;
+  }
+
+  static not(n: number) {
+    return n ? 0 : 1;
+  }
+
   static constrain(value: number, min: number, max: number) {
     if (min > max) return MathOperation.constrain(value, max, min);
     if (value < min) return min;
@@ -420,4 +495,61 @@ export class MathOperation extends Operation {
   static iif(condition: number, trueValue: number, falseValue: number) {
     return condition ? trueValue : falseValue;
   }
+
+  static switchCase(defaultValue: number, ...cases: number[]) {
+    for (let i = 0, n = cases.length; i < n; i += 2) if (cases[i]) return cases[i + 1];
+    return defaultValue;
+  }
 }
+
+export class IndicatorOperation extends Operation {
+  constructor(readonly source: Operation, readonly indicator: indicators.Indicator) {
+    super();
+  }
+}
+
+export class NumericIndicators {
+  constructor(private readonly source: Operation) {}
+
+  /**
+   * The range of a day's trading is simply _high_ - _low_.
+   * The true range extends it to yesterday's closing price if it was outside of today's range.
+   *
+   * The true range is the largest of one the following:
+   *
+   * * Most recent period's high minus the most recent period's low
+   * * Absolute value of the most recent period's high minus the previous close
+   * * Absolute value of the most recent period's low minus the previous close
+   *
+   * # Formula
+   *
+   * TR = max[(high - low), abs(high - close<sub>prev</sub>), abs(low - close<sub>prev</sub>)]
+   */
+  true_range(): IndicatorOperation {
+    return new IndicatorOperation(this.source, new indicators.TrueRange());
+  }
+
+  /**
+   * Simple moving average (SMA).
+   *
+   * # Formula
+   *
+   * ![SMA](https://wikimedia.org/api/rest_v1/media/math/render/svg/e2bf09dc6deaf86b3607040585fac6078f9c7c89)
+   *
+   * Where:
+   *
+   * * _SMA<sub>t</sub>_ - value of simple moving average at a point of time _t_
+   * * _period_ - number of periods (period)
+   * * _p<sub>t</sub>_ - input value at a point of time _t_
+   *
+   * @param period number of periods (integer greater than 0)
+   */
+  simple_moving_average(period: number): IndicatorOperation {
+    return new IndicatorOperation(this.source, new indicators.SimpleMovingAverage(period));
+  }
+}
+
+/**
+ * Indicators that require the entire data for a candle stick.
+ */
+export class CandleStickIndicators extends NumericIndicators {}
